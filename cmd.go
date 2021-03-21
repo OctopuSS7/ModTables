429e61bef20f1666ba068f9d57b7d9d4361fb471
OctopuSS7 <s.underwaterhedgehog@gmail.com>
2021-03-21 12:17:35 +0000

    Add command line flags

--- cmd.go
+++ cmd.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
+	"flag"
+//	"strconv"
 )
 
+var base int
+var mode string
+
+func parseFlags() {
+	flag.IntVar(&base, "base", 10, "Which base should this table run in?")
+	flag.StringVar(&mode, "mode", "q", "Multiplicative, Additive, or a Latin Square?")
+	flag.Parse()
+}
+
 func main() {
-	base, _ := strconv.Atoi(os.Args[2])
-	operation := os.Args[1]
-	if operation == "multiplicative" {
+	parseFlags()
+	if mode == "m" {
 		exclude := []int{}
 		generate_board(multiplicative, base, exclude)
-	} else if operation == "additive" {
+	} else if mode == "a" {
 		exclude := []int{}
 		generate_board(additive, base, exclude)
-	} else if operation == "latin_square" {
+	} else if mode == "l" {
 		exclude := append(coprime(base), 0)
 		generate_board(multiplicative, base, exclude)
 	} else {
